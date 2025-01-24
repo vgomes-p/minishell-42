c9f33fb9ec9e0e5994649b34c79687088bfe56c7
vgomes-p <vinied.personal@gmail.com>
2025-01-24 15:38:18 -0300

    colors

--- project/includes/minishell.h
+++ project/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 15:00:13 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/01/23 18:50:49 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:35:41 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,18 @@
 # include <readline/readline.h>
 
 /* DEFINES */
+# define RESET		"\033[0m"
+# define RED		"\033[1;31m"
+# define CYAN		"\033[1;36m"
+# define PINK		"\033[1;35m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define REWHITE	"\033[1;7;97m"
+# define RECYAN		"\033[1;7;36m"
+# define REPINK		"\033[1;7;35m"
+# define RERED		"\033[1;7;31m"
+# define REGREEN	"\033[1;7;32m"
+# define REYELLOW	"\033[1;7;33m"
 
 /* STRUCTURES */
 typedef struct s_minishell
