ca32e13721436c3d51fa432b0cd69a4a75a4ef98
vgomes-p <vinied.personal@gmail.com>
2025-02-10 14:27:36 -0300

    fix: cd.c, env.c, exit.c, export.c, export_utils.c, pwd.c, unset.c, exec_cmd.c and prompt.c

--- project/codes/exec/exec_cmd.c
+++ project/codes/exec/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:17:33 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/02/06 15:56:58 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/02/10 13:52:02 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,32 @@ static char	**prepare_builtin_args(t_token *tokens)
 	return (args);
 }
 
+static int	check_builtin_type(char **args, t_minishell *shell, int *ret)
+{
+	*ret = 1;
+	if (lms_strcmp(args[0], "cd") == 0)
+		ms_cd(args, shell);
+	else if (lms_strcmp(args[0], "echo") == 0)
+		ms_echo(args);
+	else if (lms_strcmp(args[0], "env") == 0)
+		ms_env(shell);
+	else if (lms_strcmp(args[0], "exit") == 0)
+		ms_exit(args, shell);
+	else if (lms_strcmp(args[0], "pwd") == 0)
+		ms_pwd(shell);
+	else if (lms_strcmp(args[0], "export") == 0)
+	{
+		ms_export(shell, args, &shell->env);
+		if (shell->error_code != 0)
+			*ret = shell->error_code;
+	}
+	else if (lms_strcmp(args[0], "unset") == 0)
+		ms_unset(shell, args, &shell->env);
+	else
+		*ret = 0;
+	return (*ret);
+}
+
 int	exec_builtin(t_token *tokens, t_minishell *shell)
 {
 	char	**args;
@@ -84,27 +110,7 @@ int	exec_builtin(t_token *tokens, t_minishell *shell)
 	args = prepare_builtin_args(tokens);
 	if (!args)
 		return (-1);
-	ret = 1;
-	if (lms_strcmp(args[0], "cd") == 0)
-		ms_cd(args);
-	else if (lms_strcmp(args[0], "echo") == 0)
-		ms_echo(args);
-	else if (lms_strcmp(args[0], "env") == 0)
-		ms_env(shell);
-	else if (lms_strcmp(args[0], "exit") == 0)
-		ms_exit(args, shell);
-	else if (lms_strcmp(args[0], "pwd") == 0)
-		ms_pwd();
-	else if (lms_strcmp(args[0], "export") == 0)
-	{
-		ms_export(shell, args, &shell->env);
-		if (shell->error_code != 0)
-			ret = shell->error_code;
-	}
-	else if (lms_strcmp(args[0], "unset") == 0)
-		ms_unset(shell, args, &shell->env);
-	else
-		ret = 0;
+	ret = check_builtin_type(args, shell, &ret);
 	free(args);
 	return (ret);
 }
