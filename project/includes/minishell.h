eddf4325be79ec3b6cfd68798ed4bcbd2ee1538e
vgomes-p <vinied.personal@gmail.com>
2025-02-12 18:00:28 -0300

    finallymente

--- project/includes/minishell.h
+++ project/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 15:00:13 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/02/11 17:32:21 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:58:22 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,19 +50,7 @@
 # define REORANGE	"\001\033[1;7;38;5;214m\002"
 # define REWHITE	"\001\033[1;7;97m\002"
 
-/* STRUCTURES */
-typedef struct s_minishell
-{
-	char	**env;
-	size_t	env_size;
-	char	*prompt;
-	int		exit_stt;
-	int		term_width;
-	int		term_height;
-	char	*error_message;
-	int		error_code;
-}	t_minishell;
-
+/* ENUM */
 typedef enum e_token_tp
 {
 	CMD,
@@ -76,12 +64,47 @@ typedef enum e_token_tp
 	SYNTAX_ERROR,
 }	t_token_tp;
 
+/* STRUCTURES */
 typedef struct s_token
 {
 	char			*value;
 	t_token_tp		type;
 	struct s_token	*next;
+	struct s_token	*prev;
 }	t_token;
+
+typedef struct s_exec
+{
+	t_token	*temp;
+	char	**cmd;
+	int		**fd;
+	int		stts;
+	pid_t	*pid;
+	int		n_pros;
+}	t_exec;
+
+typedef struct s_expand
+{
+	char	**hold_str;
+	int		ex;
+	int		start;
+	int		end;
+	int		quotes;
+	int		ex_n;
+	int		i;
+}	t_expand;
+
+typedef struct s_minishell
+{
+	char	**env;
+	char	*prompt;
+	size_t	env_size;
+	int		exit_stt;
+	int		term_width;
+	int		error_code;
+	int		term_height;
+	char	*error_message;
+}	t_minishell;
 
 /* CODES DIR */
 char		**dup_env(char **envp, size_t *envsz);
@@ -94,46 +117,54 @@ t_token		*mktoken(char *value, t_token_tp type);
 char		**ms_split_quotes(const char *input);
 t_token		*create_token_list(char **split, t_token *head);
 t_token		*tokening(char *input);
-
 //token_utils_0
 bool		process_quotes(const char *input, int *pos, bool *in_quotes,
-						char *quote_ch);
+				char *quote_ch);
 char		**process_tokens(const char *input, char **tokens);
 char		*extract_token(const char *input, int *pos);
 t_token_tp	get_token_type(char *token, t_token *current, int is_first);
-
 //token_utils_1
 bool		is_quotes(char ch);
 bool		is_operator(char *str);
 void		cleanup_tokens(char **tokens, int token_cnt);
-void		free_split_array(char **split);
+void		free_split(char **split);
 int			count_tokens(t_token *tokens);
-
 //token_util_2
 void		free_tokens(t_token *tokens);
 char		*clean_token(const char *str, int len);
-
 //parse
+int			parser(t_token **head, char *str);
 //parse_utils_0
 bool		valid_syntax(t_token *tokens);
 
-
-
-
+/* BUILDINS DIR */
+//bi_utils
 int			find_envar(const char *var, char **envp);
 void		update_envar(const char *var, int index0, char ***envp);
 int			valid_name(const char *var);
 void		export_err(const char *arg);
-
+//echo
 void		ms_echo(char **args);
+//pwd
 void		ms_pwd(t_minishell *shell);
+//ms_env
 void		ms_env(t_minishell *shell);
+//cd
 void		ms_cd(char **args, t_minishell *shell);
+//exit
 void		ms_exit(char **args, t_minishell *shell);
+//unset
 void		ms_unset(t_minishell *shell, char **args, char ***envp);
+//export
 void		ms_export(t_minishell *shell, char **args, char ***envp);
 
+/* EXEC DIR */
+//exec_externs
 void		exec_extern(t_token *tokens, t_minishell *shell);
+//exec_buildins
 int			exec_builtin(t_token *tokens, t_minishell *shell);
+//exec_utils
+char		**prepare_args(t_token *tokens);
+int			is_buildin(char *token);
 
 #endif
