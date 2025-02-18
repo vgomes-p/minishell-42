858fd8b92b1c71357092f25eabba65a649ab537a
vgomes-p <vinied.personal@gmail.com>
2025-02-18 17:29:29 -0300

    fix: memory leak
    - don't make me laugh, they were...
    
    - popular?
    
    - uhhhhh, RIGHT

--- project/codes/main.c
+++ project/codes/main.c
@@ -6,11 +6,13 @@
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:23:37 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/02/12 17:56:38 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:09:11 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+t_minishell	*g_shell = NULL;
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -21,10 +23,14 @@ int	main(int argc, char **argv, char **envp)
 	shell.env = dup_env(envp, &shell.env_size);
 	shell.prompt = NULL;
 	shell.exit_stt = 0;
+	g_shell = &shell;
+	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, handle_signal);
 	welcome();
 	while (1)
 	{
 		ms_prompt(&shell);
 	}
+	free_env(shell.env);
 	return (0);
 }
--- project/codes/utils.c
+++ project/codes/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_utils.c                                       :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:10:30 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/02/12 17:56:36 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:18:45 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,42 @@
 char	**dup_env(char **envp, size_t *envsz)
 {
 	char	**nwenv;
-	int		cnt;
-	int		pos;
+	size_t		cnt;
+	size_t		pos;
 
+	if (!envp || !envsz)
+		return (NULL);
 	cnt = 0;
-	if (envp)
-		while (envp[cnt])
-			cnt++;
+	while (envp[cnt])
+		cnt++;
 	*envsz = cnt;
 	nwenv = ft_calloc(cnt + 1, sizeof(char *));
 	if (!nwenv)
 		return (NULL);
-	pos = -1;
-	while (++pos < cnt)
+	pos = 0;
+	while (pos < cnt)
 	{
 		nwenv[pos] = ft_strdup(envp[pos]);
 		if (!nwenv[pos])
 		{
-			while (--pos >= 0)
-				free(nwenv[pos]);
+			while (--pos > 0)
+				free(nwenv[--pos]);
 			free(nwenv);
 			return (NULL);
 		}
+		pos++;
 	}
+	nwenv[cnt] = NULL;
 	return (nwenv);
+}
+
+void	handle_signal(int sig)
+{
+	(void)sig;
+	if (g_shell->env)
+	{
+		free_env(g_shell->env);
+		g_shell->env = NULL;
+	}
+	exit(0);
 }
