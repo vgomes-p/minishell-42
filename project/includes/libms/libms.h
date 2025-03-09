fc73b2348762eb97757967f344d89ff8f19b8d57
vgomes-p <vinied.personal@gmail.com>
2025-03-09 20:33:15 -0300

    feat: lms new files

--- project/includes/libms/libms.h
+++ project/includes/libms/libms.h
@@ -6,7 +6,7 @@
 /*   By: vgomes-p <vgomes-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 12:57:07 by vgomes-p          #+#    #+#             */
-/*   Updated: 2025/02/04 14:48:25 by vgomes-p         ###   ########.fr       */
+/*   Updated: 2025/03/06 13:39:47 by vgomes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int		lms_isnum(const char *str);
 char	*lms_strndup(const char *str, size_t n);
 char	*lms_strjoin_free(char *str1, char *str2);
 int		lms_strcmp(const char *s1, const char *s2);
+char	*lms_strjoin_char(char const *str0, char ch);
 char	*lms_strncpy(char *dest, const char *src, size_t n);
 void	*lms_realloc(void *ptr, size_t oldsz, size_t nwsize);
 char	*lms_strstr(const char *haystack, const char *needle);
